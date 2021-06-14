06f5700dcc49c5e564a9b6af8e01216a935f40d5
aydevelop <ay.develop1@gmail.com>
2021-06-14 11:34:13 +0300

    1. generic-list component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  movies1: any[] = [];
+  movies2: any[] = [];
+
+  ngOnInit(): void {
+    this.movies1 = [
+      {
+        title: 'Spider-Man!!!',
+        releaseDate: new Date(),
+        price: 1400,
+      },
+      {
+        title: 'Spider-Man 2',
+        releaseDate: new Date(),
+        price: 2400,
+      },
+    ];
+
+    this.movies2 = [
+      {
+        title: 'Spider-Man 222',
+        releaseDate: new Date(),
+        price: 1400,
+      },
+      {
+        title: 'Spider-Man 222',
+        releaseDate: new Date(),
+        price: 2400,
+      },
+    ];
+  }
+
   title = 'angular-movies';
-
   movie = {
-    title: "Title123"
-  }
+    title: 'Title123',
+  };
 
-  duplicateNumber(n:number){
-    return n*2;
+  duplicateNumber(n: number) {
+    return n * 2;
   }
-
 }
--- client/src/app/movies/movies-list/movies-list.component.ts
+++ client/src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-movies-list',
@@ -6,24 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movies-list.component.css'],
 })
 export class MoviesListComponent implements OnInit {
-  movies: any[] = [2];
+  @Input()
+  movies: any[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.movies = [
-        {
-          title: 'Spider-Man',
-          releaseDate: new Date(),
-          price: 1400,
-        },
-        {
-          title: 'Spider-Man 2',
-          releaseDate: new Date(),
-          price: 2400,
-        },
-      ];
-    }, 1000);
+    // setTimeout(() => {
+    //   this.movies = [
+    //     {
+    //       title: 'Spider-Man!!!',
+    //       releaseDate: new Date(),
+    //       price: 1400,
+    //     },
+    //     {
+    //       title: 'Spider-Man 2',
+    //       releaseDate: new Date(),
+    //       price: 2400,
+    //     },
+    //   ];
+    // }, 1500);
+  }
+
+  remove(index: number) {
+    this.movies.splice(index, 1);
   }
 }
