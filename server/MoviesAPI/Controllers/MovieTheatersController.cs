7599536e32e513b37b771b70f263e61d568b3821
aydevelop <juwric@gmail.com>
2021-07-17 19:24:31 +0300

    2. start crud movie theaters controller

--- server/MoviesAPI/Controllers/MovieTheatersController.cs
+++ server/MoviesAPI/Controllers/MovieTheatersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MoviesAPI.DTOs;
+using MoviesAPI.Entities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace MoviesAPI.Controllers
 {
     [ApiController]
     [Route("api/genres")]
-    public class MovieTheatersController
+    public class MovieTheatersController : Controller
     {
         private readonly ApplicationDbContext context;
         private readonly IMapper mapper;
@@ -22,10 +23,32 @@ namespace MoviesAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<MovieTheaterDTO>>> Get()
+        public async Task<ActionResult<List<MovieTheater>>> Get()
         {
             var entities = await context.MovieThreaters.OrderBy(x => x.Name).ToListAsync();
-            return mapper.Map<List<MovieTheaterDTO>>(entities);
+            return entities;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<MovieTheater>> GetAsync(int id)
+        {
+            var movieTheater = await context.MovieThreaters.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (movieTheater == null)
+            {
+                return NotFound();
+            }
+
+            return movieTheater;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(MovieTheaterCreationDTO movieCreationDTO)
+        {
+            var movieTheater = mapper.Map<MovieTheater>(movieCreationDTO);
+            context.Add(movieTheater);
+            await context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
